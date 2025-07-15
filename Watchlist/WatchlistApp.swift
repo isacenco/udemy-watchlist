b72d9de571ca9f0c45d0bca4a28e54852e02e65e
Ghenadie Isacenco <isacenco@gmail.com>
2025-07-15 16:13:29 +0200

    Project Setup, Model

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Ghenadie Isacenco on 15/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
